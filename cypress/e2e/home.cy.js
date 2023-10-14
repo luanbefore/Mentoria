f2006d5d1e533e6e2881508fd8eba20270523474
Luiz Neto <luiz.neto@before.com.br>
2023-10-14 07:38:55 -0400

    first commit

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,32 +1,23 @@
-describe('Validar teste na tela do Bugereats', () => {
-    it('validando texto principal', () => {
-        cy.visit('https://buger-eats.vercel.app/');
-        cy.get('h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats');
+describe('Validar teste de compra de novos itens ao carrinho Swag Labs', () => {
+    it('validando LOGIN no sistema', () => {
+    cy.visit('https://www.saucedemo.com/');
+    cy.get('[data-test=username]').type('standard_user');
+    cy.get('[data-test=password]').type('secret_sauce');
+    cy.get('[data-test=login-button]').click();
+    cy.get('#item_4_title_link > .inventory_item_name').click();
+    cy.get('[data-test=add-to-cart-sauce-labs-backpack]').click();
+    cy.get('.shopping_cart_link').click();
+    cy.get('[data-test=continue-shopping]').click();
+    cy.get('[data-test=add-to-cart-sauce-labs-fleece-jacket]').click();
+    cy.get('.shopping_cart_link').click();
+    cy.get('[data-test=checkout]').click();
+    cy.get('form').click();
+    cy.get('[data-test=firstName]').type('Luiz Carlos');
+    cy.get('[data-test=lastName]').type('Neto');
+    cy.get('[data-test=postalCode]').type('79013250');
+    cy.get('[data-test="continue"]').click()
+    cy.get('.summary_total_label').click();
+    cy.get('[data-test=finish]').click();
+    cy.get('.complete-header').should ('have.text', 'Thank you for your order!')
     });
-    it('validando texto secundário', () => {
-        cy.visit('https://buger-eats.vercel.app/');
-        cy.get('p').should('have.text', 'Em vez de oportunidades tradicionais de entrega de refeições em horários pouco flexíveis, seja seu próprio chefe.');
-    });
-    it('validando NOME botão cadastro', () => {
-        cy.visit('https://buger-eats.vercel.app/');
-        cy.get('strong').should('have.text', 'Cadastre-se para fazer entregas');
-    });
-    it('validando CLICAR e preencher formulário no botão cadastro', () => {
-        cy.visit('https://buger-eats.vercel.app/');
-        cy.get('a').click();
-        cy.get('[name="name"]').type('Luiz Carlos Neto');
-        cy.get('[name="email"]').type('luiz.neto@before.com.br');
-        cy.get('[name="cpf"]').type('04705938143')
-        cy.get('[name="whatsapp"]').type('67992414555')
-        cy.get('[name="postalcode"]').type('79013250')
-        cy.get('[value="Buscar CEP"]').click()
-        cy.get('[placeholder="Número"]').type('540')
-        cy.get('[placeholder="Complemento"]').type('Casa')
-        cy.get('.delivery-method > :nth-child(3)').click()
-        cy.get('.dropzone').selectFile('download (1).jpeg', { action: 'drag-drop' })
-        cy.get('.button-success').click()
-        cy.get('.swal2-popup')
-            .should('contain.text', 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.')
-            cy.get('.swal2-confirm').click()
-    });
-});
+  });
